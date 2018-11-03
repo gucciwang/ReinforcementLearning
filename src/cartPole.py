3d89eebdd966d00e96098ddd9fe22ac068bf8d92
Calvin Wang <calvin.wang0628@gmail.com>
2018-11-03 11:29:11 -0700

    Cartpole RL

---
+++ src/cartPole.py
@@ -0,0 +1,22 @@
+import gym
+
+env = gym.make('CartPole-v0')
+# env = gym.make('MountainCar-v0')
+# env = gym.make('MsPacman-v0')
+
+print(env.action_space)
+print(env.observation_space)
+print(env.observation_space.high)
+print(env.observation_space.low)
+
+env.reset()
+for i_episode in range(20):
+    observation = env.reset()
+    for t in range(100):
+        env.render()
+        print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
