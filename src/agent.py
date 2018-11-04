b2448ef7407d139e34de7fc8bf0711eaa043d7d0
Calvin Wang <calvin.wang0628@gmail.com>
2018-11-03 20:05:19 -0700

    Update formatting

--- src/agent.py
+++ src/agent.py
@@ -28,6 +28,7 @@ class A3C_Worker():
         self.a_size = args.a_size
         self.sleep_time = args.sleep_time
         self.global_episodes = global_episodes  # Number of global episodes
+        self.num_actions = 4
 
         # Setup Atari Environment
         self.env = atari_env(args.env, {'crop1': 2, 'crop2': 10, 'dimension2': 84},
@@ -38,7 +39,7 @@ class A3C_Worker():
         self.update_local_ops = self.local_net.update_network_op('global')
 
     # Train on weights
-    def train(self, sess, thread_manager, saver, max_episode_length=None, discount_factor=None):
+    def train(self, sess, thread_manager, saver=None, max_episode_length=None, discount_factor=None):
         print ("Starting worker " + str(self.number))
         action_indexes = np.arange(self.num_actions)
         done = True
@@ -60,110 +61,114 @@ class A3C_Worker():
                 sess.run(self.update_local_ops)  # Initialize weights to global network
                 batch.initial_feature = last_lstm_state
 
-                for _ in range(BATCH_SIZE):
-                    policy, value, last_lstm_state = sess.run(
-                        [self.local_net.policy, self.local_net.value, self.local_net.state_out],
-                        feed_dict={self.local_net.inputs: [state],
-                                   self.local_net.state_in[0]: last_lstm_state[0],
-                                   self.local_net.state_in[1]: last_lstm_state[1]})
+            BATCH_SIZE = 16
 
-                    value = value[0][0]  # value is 2-dimensional
-                    action = np.random.choice(action_indexes,
-                                              p=policy[0])  # pick action with given probability -> Converges fastest
-                    state, reward, done, _ = self.env.step(action)  # Pick action
+            for _ in range(BATCH_SIZE):
+                policy, value, last_lstm_state = sess.run(
+                    [self.local_net.policy, self.local_net.value, self.local_net.state_out],
+                    feed_dict={self.local_net.inputs: [state],
+                               self.local_net.state_in[0]: last_lstm_state[0],
+                               self.local_net.state_in[1]: last_lstm_state[1]})
 
-                    batch.add_data(state, action, reward, value)
+                value = value[0][0]  # value is 2-dimensional
+                action = np.random.choice(action_indexes,
+                                          p=policy[
+                                              0])  # pick action with given probability -> Converges fastest
+                state, reward, done, _ = self.env.step(action)  # Pick action
 
-                    episode_reward += reward  # Increment for summary
+                batch.add_data(state, action, reward, value)
 
-                    if done:
-                        break
+                episode_reward += reward  # Increment for summary
 
-                # Update master network
-                if not done:
-                    bootstrap_value = sess.run(self.local_net.value,
-                                               feed_dict={self.local_net.inputs: [state],
-                                                          self.local_net.state_in[0]: last_lstm_state[0],
-                                                          self.local_net.state_in[1]: last_lstm_state[1]})
-                    batch.bootstrap = bootstrap_value[0][0]
-                else:
-                    batch.bootstrap = 0.0
+                if done:
+                    break
 
-                # -----Calculate discounted rewards and advantage functionss-----------
-                batch_rewards = np.array(batch.rewards)
-                lstm_state = batch.initial_feature
+            # Update master network
+            if not done:
+                bootstrap_value = sess.run(self.local_net.value,
+                                           feed_dict={self.local_net.inputs: [state],
+                                                      self.local_net.state_in[0]: last_lstm_state[0],
+                                                      self.local_net.state_in[1]: last_lstm_state[1]})
+                batch.bootstrap = bootstrap_value[0][0]
+            else:
+                batch.bootstrap = 0.0
 
-                # Calculate discounted rewards
-                batch_discounted_rewards = np.zeros([batch.size], np.float64)
-                R = batch.bootstrap
-                for i in reversed(range(batch.size)):
-                    R = batch_rewards[i] + DISCOUNT_FACTOR * R
-                    batch_discounted_rewards[i] = R
+            # -----Calculate discounted rewards and advantage functionss-----------
+            batch_rewards = np.array(batch.rewards)
+            lstm_state = batch.initial_feature
 
-                # Calculate advantage
-                batch_advantage = np.zeros([batch.size], np.float64)
-                p_grad = batch.bootstrap
-                for i in reversed(range(batch.size)):
-                    p_grad = batch_rewards[i] + DISCOUNT_FACTOR * p_grad
-                    batch_advantage[i] = p_grad - batch.values[i]
+            # Calculate discounted rewards
+            batch_discounted_rewards = np.zeros([batch.size], np.float64)
+            R = batch.bootstrap
+            for i in reversed(range(batch.size)):
+                R = batch_rewards[i] + DISCOUNT_FACTOR * R
+                batch_discounted_rewards[i] = R
 
-                # Update network parameters
-                _, batch_loss = sess.run([self.local_net.apply_grads, self.local_net.total_loss],
-                                         feed_dict={self.local_net.state_in[0]: lstm_state[0],
-                                                    self.local_net.state_in[1]: lstm_state[1],
-                                                    self.local_net.inputs: batch.states,
-                                                    self.local_net.target_v: batch_discounted_rewards,
-                                                    self.local_net.actions: batch.actions,
-                                                    self.local_net.advantages: batch_advantage})
+            # Calculate advantage
+            batch_advantage = np.zeros([batch.size], np.float64)
+            p_grad = batch.bootstrap
+            for i in reversed(range(batch.size)):
+                p_grad = batch_rewards[i] + DISCOUNT_FACTOR * p_grad
+                batch_advantage[i] = p_grad - batch.values[i]
 
-    def test(self, sess, thread_manager, saver, args):
-        print('Test Agent')
-
-        tf.set_random_seed(1)
-        episode_reward = 0
-
-        start_time = time.time()
-        num_tests = 0
-        reward_total_sum = 0
-        last_lstm_state = self.local_net.lstm_state_init
-        sess.run(self.update_local_ops)  # Initialzie weights to global network
+            # Update network parameters
+            _, batch_loss = sess.run([self.local_net.apply_grads, self.local_net.total_loss],
+                                     feed_dict={self.local_net.state_in[0]: lstm_state[0],
+                                                self.local_net.state_in[1]: lstm_state[1],
+                                                self.local_net.inputs: batch.states,
+                                                self.local_net.target_v: batch_discounted_rewards,
+                                                self.local_net.actions: batch.actions,
+                                                self.local_net.advantages: batch_advantage})
 
 
-        state = self.env.reset()
+def test(self, sess, thread_manager, saver, args):
+    print('Test Agent')
 
-        max_score = 0
-        while not thread_manager.should_stop():
+    tf.set_random_seed(1)
+    episode_reward = 0
 
-            policy, last_lstm_state = sess.run([self.local_net.policy, self.local_net.state_out],
-                                               feed_dict={self.local_net.inputs: [state],
-                                                          self.local_net.state_in[0]: last_lstm_state[0],
-                                                          self.local_net.state_in[1]: last_lstm_state[1]})
+    start_time = time.time()
+    num_tests = 0
+    reward_total_sum = 0
+    last_lstm_state = self.local_net.lstm_state_init
+    sess.run(self.update_local_ops)  # Initialzie weights to global network
 
-            action = np.argmax(policy[0])  # pick action with highest probability
-            state, reward, done, info = self.env.step(action)  # Pick action
+    state = self.env.reset()
 
-            episode_reward += reward
-            if args.render:
-                self.env.render()
+    max_score = 0
+    while not thread_manager.should_stop():
 
-        if done and not info:
-            state = self.env.reset()
-            last_lstm_state = self.local_net.lstm_state_init
-        elif info:
-            num_tests += 1
-            reward_total_sum += episode_reward
-            reward_mean = reward_total_sum / num_tests
-            string_reward_mean = "{0:.4f}".format(reward_mean)
+        policy, last_lstm_state = sess.run([self.local_net.policy, self.local_net.state_out],
+                                           feed_dict={self.local_net.inputs: [state],
+                                                      self.local_net.state_in[0]: last_lstm_state[0],
+                                                      self.local_net.state_in[1]: last_lstm_state[1]})
 
-            # Print episode summary
-            stop_time = time.time()
-            time_elapsed = str(time.strftime("%H:%M:%S", time.gmtime(stop_time - start_time)))
-            summary = str(datetime.now())[:-7] + "  |  Elapsed: " + time_elapsed + "  |  Count: " + str(
-                num_tests) + "  |  Episode Reward: " + str(episode_reward) + "  |  Average Reward: " + string_reward_mean
-            print(summary)
-            with open("output.txt", "a") as myfile:
-                myfile.write(summary + "\n")
+        action = np.argmax(policy[0])  # pick action with highest probability
+        state, reward, done, info = self.env.step(action)  # Pick action
 
-        episode_reward = 0
+        episode_reward += reward
+        if args.render:
+            self.env.render()
+
+    if done and not info:
         state = self.env.reset()
         last_lstm_state = self.local_net.lstm_state_init
+    elif info:
+        num_tests += 1
+        reward_total_sum += episode_reward
+        reward_mean = reward_total_sum / num_tests
+        string_reward_mean = "{0:.4f}".format(reward_mean)
+
+        # Print episode summary
+        stop_time = time.time()
+        time_elapsed = str(time.strftime("%H:%M:%S", time.gmtime(stop_time - start_time)))
+        summary = str(datetime.now())[:-7] + "  |  Elapsed: " + time_elapsed + "  |  Count: " + str(
+            num_tests) + "  |  Episode Reward: " + str(
+            episode_reward) + "  |  Average Reward: " + string_reward_mean
+        print(summary)
+        with open("output.txt", "a") as myfile:
+            myfile.write(summary + "\n")
+
+    episode_reward = 0
+    state = self.env.reset()
+    last_lstm_state = self.local_net.lstm_state_init
--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ import argparse
 from random import choice
 import time
 
-# Parse Inputs - Default parameters listed below
+#Parse Inputs - Default parameters listed below
 parser = argparse.ArgumentParser(description='A3C')
 parser.add_argument('--lr', type=float, help='learning rate', default = 0.0001)
 parser.add_argument('--gamma', type=float, help='discount factor for rewards', default = 0.99)
@@ -26,24 +26,24 @@ parser.add_argument('--max-episode-length', type=int, help='maximum length of an
 parser.add_argument('--env',help='environment to train on', default = 'MsPacman-v0')
 parser.add_argument('--a-size', type=int, help='number of actions available in MsPacman env', default = 9)
 parser.add_argument('--render', help='render game', default = False)
-parser.add_argument('--sleep-time', type=float, help='time between each test (seconds)', default = 15)
+parser.add_argument('--sleep-time', type=float, help='time between each test (seconds)', default = 7)
 parser.add_argument('--skip-rate', type=int, help='frame skip rate', default = 4)
 parser.add_argument('--load', help='load existing model', default = False)
-parser.add_argument('--model-path', help='model path', default = '../models')
+parser.add_argument('--model-path', help='model path', default = '../models/model')
 args = parser.parse_args()
 
 if __name__ == '__main__':
     tf.reset_default_graph()
 
-    # Create directory to save model
+    #Create directory to save model
     if not os.path.exists(args.model_path):
         os.makedirs(args.model_path)
 
     with tf.device("/cpu:0"):
-        # Initialize global network
+        #Initialize global network
         global_network = A3C_Network('global', args.a_size, None) # Generate global network
         global_episodes = tf.Variable(0,dtype=tf.int32, name='global_episodes', trainable=False)
-        trainer = tf.train.AdamOptimizer(learning_rate=args.lr) # Use Adam Optimizer
+        trainer = tf.train.AdamOptimizer(learning_rate=args.lr) #Use Adam Optimizer
         workers = []
         args.start_time = time.time()
 
@@ -61,14 +61,14 @@ if __name__ == '__main__':
         else:
             sess.run(tf.global_variables_initializer())
 
-        thread_manager = tf.train.Coordinator() # Coordinate asynchronous threads
+        thread_manager = tf.train.Coordinator() #Coordinate asynchronous threads
 
-        # Begin asynchronous training
+        #Begin asynchronous training
         worker_threads = []
         for i in range(args.workers+1):
             if i < args.workers:
-                # t = threading.Thread(target=workers[i].train, args=(sess, thread_manager))
-                t = threading.Thread(target=workers[i].train(sess, thread_manager, saver))
+                # t = threading.Thread(target=workers[i].train, args=(sess, thread_manager, saver))
+                t = threading.Thread(target=workers[i].train(sess, thread_manager))
             else:
                 # t = threading.Thread(target=workers[i].test, args=(sess, thread_manager, saver, args))
                 t = threading.Thread(target=workers[i].test(sess, thread_manager, saver, args))
